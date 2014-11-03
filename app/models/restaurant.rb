87b2ec9b53fbfd251604e165d833142d6d402491
Scully87 <scully87.ts@googlemail.com>
2014-11-03 22:25:21 +0000

    leave average rating

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,9 @@ class Restaurant < ActiveRecord::Base
 
 	validates :name, length: {minimum: 3}, uniqueness: true
 	
+	def average_rating
+	    return 'N/A' if reviews.none?
+	    reviews.inject(0) {|memo, review| memo + review.rating} / reviews.count
+	end
+	
 end
