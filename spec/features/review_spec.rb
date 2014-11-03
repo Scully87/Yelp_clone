a50849e21e67af38bea1d8e35c5566f68b631603
Scully87 <scully87.ts@googlemail.com>
2014-11-03 16:50:22 +0000

    added reviews spec

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'reviewing' do
+ 
   before do
     Restaurant.create(name: 'KFC')
   end
@@ -11,7 +12,6 @@ describe 'reviewing' do
      fill_in "Thoughts", with: "so so"
      select '3', from: 'Rating'
      click_button 'Leave Review'
-
      expect(current_path).to eq '/restaurants'
      expect(page).to have_content('so so')
   end
